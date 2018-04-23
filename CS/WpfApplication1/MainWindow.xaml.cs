f57abcca230d27c1f45cb337297b94b800604ce7
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/WpfApplication1/MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using DevExpress.Xpf.Printing;
+using DevExpress.Xpf.Editors;
+using DevExpress.XtraReports.UI;
+using System.Data;
+
+namespace WpfApplication1 {
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window {
+        public MainWindow() {
+            InitializeComponent();
+        }
+
+        private void documentPreview1_Loaded(object sender, RoutedEventArgs e) {
+
+            DataContext = new MainWindowViewModel(new XtraReport1() { RequestParameters = false });
+        }
+
+
+        public class MainWindowViewModel {
+            public XtraReportPreviewModel ReportModel { get; set; }
+            public nwindDataSet DataSet { get; set; }
+            public DataTable Categories { get { return DataSet.Tables[0]; } }
+            public MainWindowViewModel(XtraReport report)
+            {
+                DataSet = new nwindDataSet();
+                
+                ReportModel = new XtraReportPreviewModel();
+                ReportModel.Report = report;
+
+                (report as XtraReport1).categoriesTableAdapter.Fill(DataSet.Categories);
+                report.CreateDocument(false);
+            }
+        }
+    }
+}
---
+++ VB/WpfApplication1/MainWindow.xaml.vb
@@ -0,0 +1,56 @@
+﻿Imports System
+Imports System.Collections.Generic
+Imports System.Linq
+Imports System.Text
+Imports System.Threading.Tasks
+Imports System.Windows
+Imports System.Windows.Controls
+Imports System.Windows.Data
+Imports System.Windows.Documents
+Imports System.Windows.Input
+Imports System.Windows.Media
+Imports System.Windows.Media.Imaging
+Imports System.Windows.Navigation
+Imports System.Windows.Shapes
+Imports DevExpress.Xpf.Printing
+Imports DevExpress.Xpf.Editors
+Imports DevExpress.XtraReports.UI
+Imports System.Data
+
+Namespace WpfApplication1
+    ''' <summary>
+    ''' Interaction logic for MainWindow.xaml
+    ''' </summary>
+    Partial Public Class MainWindow
+        Inherits Window
+
+        Public Sub New()
+            InitializeComponent()
+        End Sub
+
+        Private Sub documentPreview1_Loaded(ByVal sender As Object, ByVal e As RoutedEventArgs)
+
+            DataContext = New MainWindowViewModel(New XtraReport1() With {.RequestParameters = False})
+        End Sub
+
+
+        Public Class MainWindowViewModel
+            Public Property ReportModel() As XtraReportPreviewModel
+            Public Property DataSet() As nwindDataSet
+            Public ReadOnly Property Categories() As DataTable
+                Get
+                    Return DataSet.Tables(0)
+                End Get
+            End Property
+            Public Sub New(ByVal report As XtraReport)
+                DataSet = New nwindDataSet()
+
+                ReportModel = New XtraReportPreviewModel()
+                ReportModel.Report = report
+
+                TryCast(report, XtraReport1).categoriesTableAdapter.Fill(DataSet.Categories)
+                report.CreateDocument(False)
+            End Sub
+        End Class
+    End Class
+End Namespace
