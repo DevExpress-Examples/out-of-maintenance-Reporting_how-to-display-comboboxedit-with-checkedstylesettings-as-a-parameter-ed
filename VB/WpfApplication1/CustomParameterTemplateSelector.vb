fae6765d020061a2276b3d999e2b0aef7b74a9f1
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ VB/WpfApplication1/CustomParameterTemplateSelector.vb
@@ -0,0 +1,22 @@
+﻿Imports DevExpress.Xpf.Printing.Parameters
+Imports DevExpress.Xpf.Printing.Parameters.Models
+Imports System
+Imports System.Collections.Generic
+Imports System.Data
+Imports System.Linq
+Imports System.Text
+Imports System.Threading.Tasks
+
+Namespace WpfApplication1
+    Friend Class CustomParameterTemplateSelector
+        Inherits ParameterTemplateSelector
+
+        Public Overrides Function SelectTemplate(ByVal item As Object, ByVal container As System.Windows.DependencyObject) As System.Windows.DataTemplate
+            Dim parameterModel = TryCast(item, ParameterModel)
+            If parameterModel.Name = "CategoryID" Then
+                Return LookUpEditTemplate
+            End If
+            Return MyBase.SelectTemplate(item, container)
+        End Function
+    End Class
+End Namespace
---
+++ VB/WpfApplication1/MainWindow.xaml.vb
@@ -0,0 +1,31 @@
+﻿Imports System
+Imports System.Collections.Generic
+Imports System.Linq
+Imports System.Text
+Imports System.Threading.Tasks
+Imports System.Windows
+Imports System.Windows.Controls
+Imports System.Windows.Data
+Imports System.Windows.Documents
+Imports System.Windows.Input
+Imports System.Windows.Media
+Imports System.Windows.Media.Imaging
+Imports System.Windows.Navigation
+Imports System.Windows.Shapes
+Imports DevExpress.Xpf.Printing
+Imports DevExpress.Xpf.Editors
+Imports DevExpress.XtraReports.UI
+Imports System.Data
+
+Namespace WpfApplication1
+    ''' <summary>
+    ''' Interaction logic for MainWindow.xaml
+    ''' </summary>
+    Partial Public Class MainWindow
+        Inherits Window
+
+        Public Sub New()
+            InitializeComponent()
+        End Sub
+    End Class
+End Namespace
